a89657ad7b61792b4b8949b9e2a3ec2457f24aed
Elie Morisse <syniurge@gmail.com>
2018-05-12 19:31:40 -0300

    Add test for issue 17656 - Enum member circular reference error

---
+++ test/compilable/test17656.d
@@ -0,0 +1,17 @@
+enum E {
+    AAA = S.BBB
+}
+
+struct S {
+    enum SZAQ = E.AAA;
+    enum BBB = 8080;
+}
+
+enum E2 {
+    CCC = C.DDD
+}
+
+class C {
+    enum QZAS = E2.CCC;
+    enum DDD = 8080;
+}
